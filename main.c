22cc990b83e7d53a9a5b34bfe8034fa60c2002ec
Patrick Punch <punchp@oregonstate.edu>
2024-10-25 14:05:28 -0700

    completed cploration 4

--- main.c
+++ main.c
@@ -9,9 +9,33 @@
 
 int main(int argc, const char *argv[])
 {		
+	if (argc != 2)
+	{
+		printf("Usage: %s [filename]\n", argv[0]);
+		exit(EXIT_FAILURE);
+		// fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		// return EXIT_FAILURE;
+	}
 
-	// your code here
-			
+	const char *file_name = argv[1];
+	FILE *file = fopen(file_name, "r");
+	if (file == NULL)
+	{
+		perror("Unable to open file");
+		exit(EXIT_FAILURE);
+	}
+	// FILE *file = fopen(argv[1], "r");
+	// if (file == NULL)
+	// {
+	// 	perror("Error opening file");
+	// 	return EXIT_FAILURE;
+	// }
+
+	// Call the parse function
+	parse(file);
+
+	// Close the file pointer
+	fclose(file);
+
+	return 1;
 }
-
-
